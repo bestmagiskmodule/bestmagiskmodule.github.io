15b1759fd3d6205d05c6ff5c408cedb17c00e106
revWhiteShadow <89025328+White9shadow@users.noreply.github.com>
2023-12-01 15:25:33 +0600

    Update a.js

--- assets/js/a.js
+++ assets/js/a.js
@@ -1,6 +1,6 @@
 function search() {
             var searchTerm = document.getElementById("searchTerm").value;
-            var url = "google.com?search=magiskflash.com" + encodeURIComponent(searchTerm);
+            var url = "https://google.com?search=magiskflash.com " + encodeURIComponent(searchTerm);
             window.location.href = url;
         }
 	
