e0dbe6ce9105fb1f2498ed30d7cf518f22e81a74
bestmagiskmodule <67999331+bestmagiskmodule@users.noreply.github.com>
2023-12-01 13:15:16 +0600

    Update a.js

--- assets/js/a.js
+++ assets/js/a.js
@@ -1,6 +1,6 @@
 function search() {
             var searchTerm = document.getElementById("searchTerm").value;
-            var url = "https://https://bestmagiskmodule.github.io/?search=" + encodeURIComponent(searchTerm);
+            var url = "https://bestmagiskmodule.github.io/?search=" + encodeURIComponent(searchTerm);
             window.location.href = url;
         }
 	
